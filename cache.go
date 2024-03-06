a6d591ec1794d3f175f4e21a877cfde8baba6a15
Shan Lu <lushan0903@gmail.com>
2024-03-05 22:38:35 -0800

    encapsulate LRU using mutex

---
+++ cache.go
@@ -0,0 +1,35 @@
+package distributedcache
+
+import (
+	"lru/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	if c.lru == nil { //lazy initialization
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+	c.mu.Unlock()
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+
+	return
+}
